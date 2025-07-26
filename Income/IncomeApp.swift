f5c74ba0c57b390996f982b3a2895320477b016c
Maimai10808 <331686549@qq.com>
2025-07-27 04:06:50 +0800

    1.init

---
+++ Income/IncomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  IncomeApp.swift
+//  Income
+//
+//  Created by mac on 7/27/25.
+//
+
+import SwiftUI
+
+@main
+struct IncomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
