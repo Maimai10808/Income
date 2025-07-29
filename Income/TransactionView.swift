4be7e83b6535007fb483acf0f46694ddfde507a0
Maimai10808 <331686549@qq.com>
2025-07-29 10:00:53 +0800

    5. TransactionView SubView

---
+++ Income/TransactionView.swift
@@ -0,0 +1,61 @@
+//
+//  TransactionView.swift
+//  Income
+//
+//  Created by mac on 7/29/25.
+//
+
+import SwiftUI
+
+struct TransactionView: View {
+    let transaction: Transaction
+    
+    
+    
+    var body: some View {
+        VStack {
+            HStack {
+                
+                Spacer()
+                
+                Text(transaction.displayDate)
+                    .font(.system(size: 14))
+                
+                Spacer()
+                
+            }
+            .padding(.vertical, 5)
+            .background(Color.lightGrayShade.opacity(0.5))
+            .clipShape(RoundedRectangle(cornerRadius: 5))
+            
+            HStack {
+                Image(systemName: transaction.type == .income ?
+                      "arrow.up.forward" :
+                        "arrow.down.forward")
+                .font(.system(size: 16, weight: .bold))
+                .foregroundStyle(transaction.type == .income ?
+                                 Color.green :
+                                    Color.red)
+                
+                VStack(alignment: .leading, spacing: 5) {
+                    
+                    HStack {
+                        Text(transaction.title)
+                            .font(.system(size: 15, weight: .bold))
+                        Spacer()
+                        Text(String(transaction.displayAmout))
+                            .font(.system(size: 15, weight: .bold))
+                    }
+                    Text("Completed")
+                        .font(.system(size: 14))
+                    
+                }
+            }
+        }
+        .listRowSeparator(.hidden)
+    }
+}
+
+#Preview {
+    TransactionView(transaction: Transaction(title: "Apple", type: .expense, amount: 5.00, date: Date()))
+}
