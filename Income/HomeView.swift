583aa12db2350b8e2b9f82575f0bcaae14495f7f
Maimai10808 <331686549@qq.com>
2025-07-29 10:31:32 +0800

    6. BalanceView

--- Income/HomeView.swift
+++ Income/HomeView.swift
@@ -16,8 +16,72 @@ struct HomeView: View {
     ]
     
     
+    fileprivate func BalanceView() -> some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 8)
+                .fill(Color.primaryLightGreen)
+            
+            VStack(alignment: .leading) {
+                
+                HStack {
+                    
+                    VStack {
+                        
+                        Text("BALANCE")
+                            .font(.caption)
+                            .foregroundStyle(Color.white)
+                        
+                        Text("$2")
+                            .font(.system(size: 42, weight: .light))
+                            .foregroundStyle(Color.white)
+                    }
+                    
+                    Spacer()
+                }
+                .padding(.top)
+                
+                
+                
+                HStack(spacing: 25) {
+                    VStack(alignment: .leading, spacing: 5) {
+                        Text("Expense")
+                            .font(.system(size: 15, weight: .semibold))
+                            .foregroundStyle(Color.white)
+                        
+                        Text("$22")
+                            .font(.system(size: 15, weight: .regular))
+                            .foregroundStyle(Color.white)
+                        
+                    }
+                    
+                    VStack {
+                        Text("Income")
+                            .font(.system(size: 15, weight: .semibold))
+                            .foregroundStyle(Color.white)
+                        
+                        Text("$22")
+                            .font(.system(size: 15, weight: .regular))
+                            .foregroundStyle(Color.white)
+                        
+                    }
+                    
+                }
+                
+                Spacer()
+                
+            }
+            .padding(.horizontal)
+        }
+        .shadow(color: Color.black.opacity(0.3), radius: 10, x:0, y: 5)
+        .frame(height: 150)
+        .padding(.horizontal)
+    }
+    
+    
     var body: some View {
         VStack {
+            BalanceView()
+            
             List {
                 ForEach(transactions) { transaction in
                     
