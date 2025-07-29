cc3be6cc65261aa0ad871791a49228b5b4e3e0aa
Maimai10808 <331686549@qq.com>
2025-07-29 09:38:07 +0800

    3. Transaction Row

--- Income/ContentView.swift
+++ Income/ContentView.swift
@@ -19,9 +19,46 @@ struct ContentView: View {
         VStack {
             List {
                 ForEach(transactions) { transaction in
-                    HStack {
-                        
+                    
+                    VStack {
+                        HStack {
+                            
+                            Spacer()
+                            
+                            Text("13/11/24")
+                                .font(.system(size: 14))
+                            
+                            Spacer()
+                            
                         }
+                        .padding(.vertical, 5)
+                        .background(Color.lightGrayShade.opacity(0.5))
+                        .clipShape(RoundedRectangle(cornerRadius: 5))
+                        
+                        HStack {
+                            Image(systemName: transaction.type == .income ?
+                                  "arrow.up.forward" :
+                                    "arrow.down.forward")
+                            .font(.system(size: 16, weight: .bold))
+                            .foregroundStyle(transaction.type == .income ?
+                                             Color.green :
+                                                Color.red)
+                            
+                            VStack(alignment: .leading, spacing: 5) {
+                                
+                                HStack {
+                                    Text(transaction.title)
+                                        .font(.system(size: 15, weight: .bold))
+                                    Spacer()
+                                    Text(String(transaction.amount))
+                                        .font(.system(size: 15, weight: .bold))
+                                }
+                                Text("Completed")
+                                    .font(.system(size: 14))
+                                
+                            }
+                        }
+                    }
                     }
                 }
             }
